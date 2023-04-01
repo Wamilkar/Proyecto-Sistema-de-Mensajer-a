6b1b207abf0ae056a5a4bb5db182853202f97bb1
Wamilkar <amkc008@gmail.com>
2023-03-31 20:44:25 -0600

    Se cambia la autenticación por número télefonico

---
+++ WhatsAppClone/app/src/main/java/com/g6/whatsappclone/VerificationActivity.kt
@@ -0,0 +1,36 @@
+package com.g6.whatsappclone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.g6.whatsappclone.databinding.ActivityVerificationBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class VerificationActivity : AppCompatActivity() {
+
+    var binding : ActivityVerificationBinding? = null
+
+    var auth:FirebaseAuth? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityVerificationBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        auth = FirebaseAuth.getInstance()
+        if (auth!!.currentUser != null){
+            val intent = Intent(this@VerificationActivity,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        supportActionBar?.hide()
+        binding!!.editNumber.requestFocus()
+        binding!!.ContinueBtn.setOnClickListener{
+            val intent = Intent(this@VerificationActivity,
+                OTPActivity::class.java)
+            intent.putExtra("phoneNumber", binding!!.editNumber.text.toString())
+            startActivity(intent)
+        }
+    }
+}
